843af46ca4c15b2f565a3ece2c581b0bba3d21d9
Rajkumar Das <dasrajkumar52@gmail.com>
2022-08-14 09:48:25 +0530

    more cleaned code

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { PostAuthor } from './PostAuthor'
+import { selectAllPosts } from './postsSlice'
 
 export const PostsList = () => {
 
-    const posts = useSelector(state => state.posts)
+    const posts = useSelector(selectAllPosts)
 
     const renderedPosts = posts.map(post => (
         <article className="post-excerpt" key={post.id}>
             <h3>{post.title}</h3>
             <p className="post-content">{post.content.substring(0, 100)}</p>
+            <PostAuthor userId={post.userId} />
             <Link to={`/posts/${post.id}`} className="button muted-button">View Post</Link>&nbsp;
             <Link to={`/editPost/${post.id}`} className="button muted-button">Edit Post</Link>&nbsp;
             <Link to={`/deletePost/${post.id}`} className="button muted-button">Delete Post</Link>
